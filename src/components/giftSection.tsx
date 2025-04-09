81c6d744c1fafca35ca8483193a5ec0d431508cd
sergioormeno <felipesolflex@gmail.com>
2025-04-09 08:52:16 -0400

    update button

--- src/components/giftSection.tsx
+++ src/components/giftSection.tsx
@@ -117,16 +117,19 @@ export default function GiftSection() {
                         </div>
                       ))}
 
-                      {/* Botón copiar todos */}
-                      <div className="text-center pt-4">
+                      {/* Botón copiar todos (con solo ícono) */}
+                      <div className="flex justify-end pt-2">
                         <button
                           onClick={copiarTodosLosDatos}
-                          className="btn-primary px-4 py-2 text-sm rounded-full"
+                          className="text-[var(--color-text)] hover:text-[var(--color-accent)] transition"
+                          title="Copiar todos los datos"
                         >
-                          Copiar todos los datos
+                          <Copy className="w-5 h-5" />
                         </button>
                       </div>
                     </div>
+
+
                   </div>
                 )}
 
@@ -171,7 +174,19 @@ export default function GiftSection() {
                                 </div>
                               </div>
                             ))}
+
+                            {/* Botón copiar todos (con solo ícono) */}
+                            <div className="flex justify-end pt-2">
+                              <button
+                                onClick={copiarTodosLosDatos}
+                                className="text-[var(--color-text)] hover:text-[var(--color-accent)] transition"
+                                title="Copiar todos los datos"
+                              >
+                                <Copy className="w-5 h-5" />
+                              </button>
+                            </div>
                           </div>
+
                           <p className="text-sm">O bien puedes colaborar con tarjeta de crédito aquí:</p>
                           <a
                             href={deseos[selectedDeseo].link}
